1b9e0d7cfc6e8b169ca3365df87055bc9d0529b8
MauroS5 <52974793+MauroS5@users.noreply.github.com>
2019-11-14 14:52:30 +0100

    2.0.1
    Fixed little bug

--- crashdetect.sh
+++ crashdetect.sh
@@ -2,6 +2,6 @@
 while [ ! $(pidof 9hmultiss) ]; do
         killall 9htl 9hviewer 9hbrowser 9hmultiss
         Xvfb :1 &
-        export DISPLAY=:1 && /root/9HitsViewer_x64/9h_start.sh > /dev/null
+        export DISPLAY=:1 && /root/9Hits/9HitsViewer_x64/9h_start.sh > /dev/null
         exit
 done
